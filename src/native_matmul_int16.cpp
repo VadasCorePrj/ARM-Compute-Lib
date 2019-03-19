f083bc97dd9fd2d0021b0c63dd1f0463e011c982
VadasCorePrj <tmlee@vadas.co.kr>
2019-03-19 17:21:23 +0900

    Edit order of argumets

---
+++ src/native_matmul_int16.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include <ctime>
+
+#define DMSG(x) do { std::cout << x << std::endl; } while(0)
+
+int M = -1;
+int N = -1;
+int K = -1;
+int repeat = 0;
+
+int16_t** src0;
+int16_t** src1;
+int16_t** dst;
+
+void fillTensor(int16_t** src, int row, int col)
+{
+	std::srand(time(NULL)/col);
+
+	// allocate & fill memory
+	for(int y = 0; y < row; y++)
+	{
+		src[y] = (int16_t*)malloc(sizeof(int16_t)*col);	
+		for(int x = 0; x < col; x++)
+		{
+			src[y][x] = (int16_t)(((static_cast<float>(rand()) / static_cast<float>(RAND_MAX/2)) - 1) * 10);
+		}
+	}
+}
+
+void freeTensor(int16_t** src, int row, int col)
+{
+	for(int y = 0; y < row; y++)
+	{
+		free(src[y]);
+	}
+	free(src);
+}
+
+void printTensor(int16_t** src, const int row, const int col)
+{
+	for(int y = 0; y < row; y++)
+	{
+		int16_t* tmp = src[y];
+		for(int x = 0; x < col; x++)
+		{
+			std::cout << static_cast<int16_t>(tmp[x]) << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+// matmul: src1 x src0 = dst
+// return average time
+double matmul(int repeat)
+{
+	double time = 0;
+	for(int iter = 0; iter < repeat; iter++)
+	{
+		double ts1 = clock();
+		for(int i = 0; i < N; i++)
+		{
+			for(int l = 0; l < M; l++)
+			{
+				int16_t val = 0.0f;
+				for(int j = 0; j < K; j++)
+				{
+					val += src0[i][j] * src1[j][l];
+				}
+				dst[i][l] = val;
+			}
+		}
+		double ts2 = clock();
+		time += (ts2 - ts1);
+	}
+
+	// Get average second for executing
+	time = time / CLOCKS_PER_SEC / repeat;
+
+
+	// Return nsec
+	return time * 1e+9;
+}
+
+/*
+ * A x B -> C
+ * src1 = A, src0=B, dst=C
+ * $ nice -2 ./native_matmul [B's col] [A's row] [A's col]
+ */
+int main(int argc, char** argv)
+{
+	if(argc < 5)
+	{
+		DMSG("Usage: ./native_matmul M N K Repeat");
+
+		return 1;
+	}
+
+	N = atoi(argv[1]);
+	K = atoi(argv[2]);
+	M = atoi(argv[3]);
+	repeat = atoi(argv[4]);
+
+	// init & fill the tensor
+	src0 = (int16_t**)malloc(sizeof(int16_t*)*N);
+	src1 = (int16_t**)malloc(sizeof(int16_t*)*K);
+	dst = (int16_t**)malloc(sizeof(int16_t*)*N);
+
+	fillTensor(src0, N, K);
+	fillTensor(src1, K, M);
+	fillTensor(dst , N, M);
+
+	// matrix multiplication
+	double time = matmul(repeat);
+
+	DMSG("nativ C++ (" << N << "x" << K << ") x (" << K << ", " << M << ") time: " << time << "ns");
+
+	freeTensor(src0, N, K);
+	freeTensor(src1, K, M);
+	freeTensor(dst , N, M);
+
+	return 0;
+}
---
+++ src/native_matmul_int32.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include <ctime>
+
+#define DMSG(x) do { std::cout << x << std::endl; } while(0)
+
+int M = -1;
+int N = -1;
+int K = -1;
+int repeat = 0;
+
+int** src0;
+int** src1;
+int** dst;
+
+void fillTensor(int** src, int row, int col)
+{
+	std::srand(time(NULL)/col);
+
+	// allocate & fill memory
+	for(int y = 0; y < row; y++)
+	{
+		src[y] = (int*)malloc(sizeof(int)*col);	
+		for(int x = 0; x < col; x++)
+		{
+			src[y][x] = (int)(((static_cast<float>(rand()) / static_cast<float>(RAND_MAX/2)) - 1) * 10);
+		}
+	}
+}
+
+void freeTensor(int** src, int row, int col)
+{
+	for(int y = 0; y < row; y++)
+	{
+		free(src[y]);
+	}
+	free(src);
+}
+
+void printTensor(int** src, const int row, const int col)
+{
+	for(int y = 0; y < row; y++)
+	{
+		int* tmp = src[y];
+		for(int x = 0; x < col; x++)
+		{
+			std::cout << tmp[x] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+// matmul: src1 x src0 = dst
+// return average time
+double matmul(int repeat)
+{
+	double time = 0;
+	for(int iter = 0; iter < repeat; iter++)
+	{
+		double ts1 = clock();
+		for(int i = 0; i < N; i++)
+		{
+			for(int l = 0; l < M; l++)
+			{
+				int val = 0.0f;
+				for(int j = 0; j < K; j++)
+				{
+					val += src0[i][j] * src1[j][l];
+				}
+				dst[i][l] = val;
+			}
+		}
+		double ts2 = clock();
+		time += (ts2 - ts1);
+	}
+
+	// Get average second for executing
+	time = time / CLOCKS_PER_SEC / repeat;
+
+
+	// Return nsec
+	return time * 1e+9;
+}
+
+/*
+ * A x B -> C
+ * src1 = A, src0=B, dst=C
+ * $ nice -2 ./native_matmul [B's col] [A's row] [A's col]
+ */
+int main(int argc, char** argv)
+{
+	if(argc < 5)
+	{
+		DMSG("Usage: ./native_matmul M N K Repeat");
+
+		return 1;
+	}
+
+	N = atoi(argv[1]);
+	K = atoi(argv[2]);
+	M = atoi(argv[3]);
+	repeat = atoi(argv[4]);
+
+	// init & fill the tensor
+	src0 = (int**)malloc(sizeof(int*)*N);
+	src1 = (int**)malloc(sizeof(int*)*K);
+	dst = (int**)malloc(sizeof(int*)*N);
+
+	fillTensor(src0, N, K);
+	fillTensor(src1, K, M);
+	fillTensor(dst , N, M);
+
+	// matrix multiplication
+	double time = matmul(repeat);
+
+	DMSG("nativ C++ (" << N << "x" << K << ") x (" << K << ", " << M << ") time: " << time << "ns");
+
+	freeTensor(src0, N, K);
+	freeTensor(src1, K, M);
+	freeTensor(dst , N, M);
+
+	return 0;
+}
